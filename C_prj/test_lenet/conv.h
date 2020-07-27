b38ade1e6ba64be6a52d71096d9a33e6e05020e3
TWW <M201972649@hust.edu.cn>
2020-07-27 13:44:49 +0800

    结果有误 需要调试

--- C_prj/test_lenet/conv.h
+++ C_prj/test_lenet/conv.h
@@ -4,6 +4,10 @@
 #include "assert.h"
 #include <stdint.h>
 #include <string.h> 
-
+void conv_1_pool2(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM, float* W2_P_DRAM, float* Bias2_P_DRAM);
+void conv_3_pool4(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM, float* W2_P_DRAM, float* Bias2_P_DRAM);
+void conv_5(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM);
+void fc_6(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM);
+void fc_7(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM);
 
 #endif
--- C_prj/test_lenet/main.cpp
+++ C_prj/test_lenet/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>  
+#include <iomanip>
 #include <fstream>  
 #include <sstream>  
 #include "stdlib.h"
@@ -7,13 +8,15 @@
 #include <opencv2/core/core.hpp>  
 #include <opencv2/highgui/highgui.hpp>  
 #include <opencv2/imgproc/imgproc.hpp>
+#include "conv.h"
 using namespace std;
 using namespace cv;
 float input[32][32] = {0};
-float C1_DRAM[6][28][28] = { 0 };
+
 float S2_DRAM[6][14][14] = { 0 };
-float C3_DRAM[16][10][10] = { 0 };
+
 float S4_DRAM[16][5][5] = { 0 };
+
 float C5_DRAM[120] = { 0 };
 float F6_DRAM[84] = { 0 };
 float F7_DRAM[10] = { 0 };
@@ -39,19 +42,45 @@ float b_fc7[10] = { 0 };
 
 void read_parameters();
 
-uint8_t pic_in[32][32] = {0};
+float pic_in[32][32] = {0};
+ 
 int main()
 {
 	read_parameters();
-	Mat img32 = imread("..\\..\\dataset\\test.bmp", 0);//读取原图
+	Mat img32 = imread("..\\..\\dataset\\test3.bmp", 0);//读取原图
 
 	for (int row = 0; row < 32; row++)      //行
 		for (int col = 0; col < 32; col++)  //列
 			{
-				pic_in[row][col] = img32.at<uchar>(row, col);
+				pic_in[row][col] = (float)(img32.at<uchar>(row, col))  /  255.0*2  -1; //归一化到-1 ~ 1 
 			}
 
+	//for (int row = 0; row < 32; row++)      //行
+	//{
+	//	for (int col = 0; col < 32; col++)  //列
+	//	{
+	//		cout << setprecision(1) << pic_in[row][col]<<' ';
+	//	}
+	//	cout << endl;
+	//}
 
+
+	conv_1_pool2(&pic_in[0][0],&W_CONV1[0][0][0][0],&S2_DRAM[0][0][0], b_conv1, W_POOL2, b_pool2);
+	conv_3_pool4(&S2_DRAM[0][0][0], &W_CONV3[0][0][0][0], &S4_DRAM[0][0][0], b_conv3, W_POOL4, b_pool4);
+	conv_5(&S4_DRAM[0][0][0], &W_CONV5[0][0][0][0], &C5_DRAM[0], b_conv5);
+	fc_6(C5_DRAM, WFC6, F6_DRAM, b_fc6);
+	fc_7(F6_DRAM, WFC7, F7_DRAM, b_fc7);
+
+	int max_arg = 0;
+	float max = 0;
+	for (int i = 0; i < 10; i++) 
+	{
+		if (F7_DRAM[i] > max)
+		{
+			max_arg = i;
+			max = F7_DRAM[i];
+		}
+	}
 	imshow("img32", img32);
 	waitKey(1000);
 	while (1);
