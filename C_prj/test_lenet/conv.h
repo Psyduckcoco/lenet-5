2c4525b19c55087d7b61429d0f79da862e2ba357
TWW <M201972649@hust.edu.cn>
2020-07-28 15:04:43 +0800

    删掉各层函数，写到一个top里，便于修改接口

--- C_prj/test_lenet/conv.h
+++ C_prj/test_lenet/conv.h
@@ -7,11 +7,5 @@
 void top_fun(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM, int8_t layer);
 
 
-void conv_1_pool2(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM);
-void conv_3_pool4(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM);
-void conv_5(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM);
-void fc_6(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM);
-void fc_7(float* In_DRAM, float* W_DRAM, float* Out_DRAM, float* Bias_DRAM);
-
 
 #endif
